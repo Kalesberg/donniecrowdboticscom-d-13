33b62ab3ba4c0b2600e953ee3474dfdaac1f537b
Donnie Kim <stardonnie@gmail.com>
2019-02-06 19:55:54 +0000

    initial commit

---
+++ android/app/src/main/java/com/donniecrowdboticscom_d_13/MainActivity.java
@@ -0,0 +1,15 @@
+package com.donniecrowdboticscom_d_13;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "donniecrowdboticscom_d_13";
+    }
+}
